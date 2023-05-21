7a2fe1700cd66000c72bb342a8bcc71dcbd4994c
shockolavawave <atharv.sambarat@gmail.com>
2023-05-21 19:39:23 +0530

    plotting

--- main.r
+++ main.r
@@ -13,3 +13,11 @@ library(tidyverse)
 df_merge <- merge(amphibio, osu_occ,
                   by.x = "Species",
                   by.y = "species")
+
+#plot object
+gg <- ggplot(df_merge, aes(x = Body_size_mm, y = Body_mass_g)) +
+    scale_x_continuous(trans = "log2") +
+    scale_y_continuous(trans = "log2")
+
+gg +
+    geom_point()
