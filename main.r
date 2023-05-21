e8494a14054c0e44d1d8ce3bf22666a921e15ce9
shockolavawave <atharv.sambarat@gmail.com>
2023-05-21 18:35:51 +0530

    Merging data frames by species

--- main.r
+++ main.r
@@ -4,3 +4,12 @@
 
 amphibio <- read.csv("files/AmphiBIO_v1.csv")
 osu_occ <- read.csv("files/OSU_amph.csv", sep = '\t')
+
+# setting up tidyverse
+install.packages("tidyverse")
+library(tidyverse)
+
+# merging data frames
+df_merge <- merge(amphibio, osu_occ,
+                  by.x = "Species",
+                  by.y = "species")
